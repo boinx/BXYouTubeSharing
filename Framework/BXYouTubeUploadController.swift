6fedcbd1ec8797857b264ba6d75e4939c698e9cc
Peter Baumgartner <peter@boinx.com>
2019-05-04 14:01:58 +0200

    Changed signature so that trailing closure can be used

--- Framework/BXYouTubeUploadController.swift
+++ Framework/BXYouTubeUploadController.swift
@@ -150,13 +150,13 @@ public class BXYouTubeUploadController: NSObject
 	/// Retrieves the list of categories that are known to YouTube. Specify a language code like "en" or "de"
 	/// to localize the names.
 	
-	public func categories(for languageCode:String, completionHandler: @escaping ([Category], Error?)->Void, maxRetries: Int = 3)
+	public func categories(for languageCode:String, maxRetries: Int = 3, completionHandler: @escaping ([Category], Error?)->Void)
 	{
         guard let accessToken = self.accessToken else
         {
             BXYouTubeAuthenticationController.shared?.requestAccessToken(completionHandler: { (accessToken, error) in
                 self.accessToken = accessToken
-                self.categories(for: languageCode, completionHandler: completionHandler, maxRetries: maxRetries - 1)
+                self.categories(for: languageCode, maxRetries: maxRetries-1, completionHandler: completionHandler)
             })
             return
         }
@@ -177,7 +177,7 @@ public class BXYouTubeUploadController: NSObject
                         // Retry
                         BXYouTubeAuthenticationController.shared?.requestAccessToken(completionHandler: { (accessToken, error) in
                             self.accessToken = accessToken
-                            self.categories(for: languageCode, completionHandler: completionHandler, maxRetries: maxRetries - 1)
+                            self.categories(for: languageCode, maxRetries: maxRetries-1, completionHandler: completionHandler)
                         })
                         return
                     }
